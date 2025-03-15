718dfb38bcd63bb792c5ca267b18f2daadaaf8d7
Igor <suportenik@gmail.com>
2025-03-15 19:21:25 +0300

    third commit

--- styles/Table.styles.js
+++ styles/Table.styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledTable = styled.table`
   width: 100%;
+  border-collapse: collapse;
 
   th {
     text-align: left;
@@ -13,5 +14,17 @@ export const StyledTable = styled.table`
 
   td {
     border-top: 1px solid rgba(0, 0, 0, 0.1);
+    padding: 8px;
+  }
+
+  @media screen and (max-width: 400px) {
+    th {
+      font-size: 0.6rem;
+    }
+
+    td {
+      font-size: 0.8rem;
+      padding: 5px;
+    }
   }
 `;
